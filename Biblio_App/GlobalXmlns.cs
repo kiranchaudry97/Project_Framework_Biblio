29d2861ff2661f457b8bc0505451c3ea23ecb482
kiranchaudry97 <kiran.chaudry@outlook.be>
2025-11-14 12:32:44 +0100

    Biblio app MAUI aanmaken

---
+++ Biblio_App/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Biblio_App")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Biblio_App.Pages")]
