87c4fc56787b1b34395b1794c5bab6562e1ae4b0
kiranchaudry97 <kiran.chaudry@outlook.be>
2025-12-08 01:36:14 +0100

    toegevoegde/verbeterde bindingconverters voor UI-logica (StringNotEmptyToBoolConverter, InverseBooleanConverter, DateToBoolConverter).

---
+++ Biblio_App/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace Biblio_App.Converters
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+                return !b;
+
+            // If value is null or not bool, treat as false -> inverse true
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+                return !b;
+
+            return false;
+        }
+    }
+}
