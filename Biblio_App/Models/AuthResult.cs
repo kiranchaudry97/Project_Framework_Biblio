53659e47c4d0588359cbcc23eae386b5fd7724e7
kiranchaudry97 <kiran.chaudry@outlook.be>
2025-12-15 22:02:17 +0100

    refreshtoken toevoegen

--- Biblio_App/Models/AuthResult.cs
+++ Biblio_App/Models/AuthResult.cs
@@ -5,6 +5,7 @@ namespace Biblio_App.Models
     public class AuthResult
     {
         public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
         public DateTime? Expires { get; set; }
         public bool Success => !string.IsNullOrEmpty(AccessToken);
     }
