b4666cdaa7a1fab0b04d95ec9ed5d383f54592aa
kiranchaudry97 <kiran.chaudry@outlook.be>
2025-12-18 22:49:21 +0100

    fix juiste mail

--- Biblio_App/Pages/Account/LoginPage.xaml.cs
+++ Biblio_App/Pages/Account/LoginPage.xaml.cs
@@ -8,12 +8,30 @@ namespace Biblio_App.Pages.Account
         public LoginPage()
         {
             InitializeComponent();
+
+#if DEBUG
+            DevPanel.IsVisible = true;
+#else
+            DevPanel.IsVisible = false;
+#endif
         }
 
         private async void OnLoginClicked(object sender, System.EventArgs e)
         {
             // Navigeer naar de web-login of toon een korte melding aan de gebruiker.
             await DisplayAlert("Login", "Gebruik de web-app voor inloggen.", "OK");
+        }
+
+        private void OnAutofillAdminClicked(object sender, System.EventArgs e)
+        {
+            EmailEntry.Text = "admin@biblio.local";
+            PasswordEntry.Text = "Admin1234?";
+        }
+
+        private void OnAutofillMedewerkerClicked(object sender, System.EventArgs e)
+        {
+            EmailEntry.Text = "medewerk@biblio.local";
+            PasswordEntry.Text = "test1234?";
         }
     }
 }
